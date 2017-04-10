41470602d24a9ddcf42736c6f3a0466a36f718fe
yanger <977865384@qq.com>
2017-04-10 11:10:12 +0800

    first commit

--- app/src/main/java/com/beicai/yyvoice/MainActivity.java
+++ app/src/main/java/com/beicai/yyvoice/MainActivity.java
@@ -2,6 +2,7 @@ package com.beicai.yyvoice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"这是主页面",Toast.LENGTH_LONG).show();
     }
 }
